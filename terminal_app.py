a3fe2e2403088b4f3587887f732396199e65ec50
KC <kshitij.choubey.1331@gmail.com>
2022-09-28 16:31:09 +0530

    new file created

---
+++ terminal_app.py
@@ -0,0 +1,11 @@
+import os
+def shell():
+    user=input("localhost login :")
+    input("["+user+"@localhost ~ ] $ ")
+
+def create_file_system():
+
+def create_user():
+def user_database_file():
+def search_user_in_database():
+def 
